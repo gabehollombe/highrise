fd662c9cec8a2ef02fa225cdb96a59698075b3d7
Gabe <gabe@avantbard.com>
2011-05-04 18:12:05 +1000

    add subject field class

---
+++ lib/highrise/subject_field.rb
@@ -0,0 +1,10 @@
+module Highrise
+  class SubjectField < Base
+    include Pagination
+
+    # def comments
+    #   Comment.find(:all, :from => "/notes/#{id}/comments.xml")
+    # end
+  end
+end
+
